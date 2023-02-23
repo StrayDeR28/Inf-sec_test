fdd7966ead3ea47f3bb2bafb72d76f8b8509fbbb
Фёдор <fedpr.pavlov.02@mail.ru>
2023-02-23 21:20:44 +0700

    Updated Registration Screne

--- Assets/Scripts/RegistrationScripts/RegistrationInfo.cs
+++ Assets/Scripts/RegistrationScripts/RegistrationInfo.cs
@@ -8,31 +8,59 @@ public class RegistrationInfo : MonoBehaviour
 {
     [SerializeField] private TMP_InputField login;
     [SerializeField] private TMP_InputField password;
+    [SerializeField] private TMP_InputField password2;
+    [SerializeField] private TMP_InputField uName;
+    [SerializeField] private TMP_InputField surname;
+    [SerializeField] private TMP_InputField patronymic;
+    [SerializeField] private TMP_Text debugText;
+
 
     public void SendLogin()
     {
         //код Германа
-        print("login:"+login.text);
+        print("login: "+login.text);
     }
     public void SendPassword()
     {
         //код Германа
-        print("password"+password.text);
+        print("password: "+password.text);
+    }
+    public void SendPassword2()
+    {
+        //код Германа
+        print("password2: " + password2.text);
     }
     public void SendName()
     {
         //код Германа
+        print("Name: " + uName.text);
     }
     public void SendSurname()
     {
         //код Германа
+        print("Surname: " + surname.text);
     }
     public void SendPatronymic()
     {
         //код Германа
+        print("Patronymic: " + patronymic.text);
     }
     public void BackendReport()
     {
         // метод для вывода информации о регистрации (успешно, не успешно...)
+        debugText.text = "Регистрация успешна";
+    }
+    public void ComparePassword()
+    {
+        if(password.text==password2.text)
+        {
+            SendPassword();
+            //код Германа, отправляющий пароль
+            debugText.text = "";
+        }
+        else
+        {
+            debugText.text = "Пароли не совпадают";
+        }
     }
 }
