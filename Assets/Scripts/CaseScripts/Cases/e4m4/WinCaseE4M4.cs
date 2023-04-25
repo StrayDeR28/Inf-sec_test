389318a32094e3cb4443b5edf2506ee4aea382b8
nickname838 <german.galitskov@gmail.com>
2023-04-25 19:00:37 +0700

    e4m1 e4m3 e4m4 done

---
+++ Assets/Scripts/CaseScripts/Cases/e4m4/WinCaseE4M4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinCaseE4M4 : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField answerInputField;
+
+    public void CaseWin()
+    {
+        if(answerInputField.text == "4")
+        {
+            print("крутой");
+            //gameObject.GetComponent<WinCase>().WinCasePlayerPrefs();
+        } else print("лох");
+    }
+}
