863d74478e1641f24a3c5052a7dafb38421a0263
FndraZ <davydkin.andrei@gmail.com>
2023-05-01 00:30:04 +0700

    fix

--- Assets/Scripts/CaseScripts/Cases/e3m1/WinCaceE3M1.cs
+++ Assets/Scripts/CaseScripts/Cases/e3m1/WinCaceE3M1.cs
@@ -8,7 +8,7 @@ public class WinCaceE3M1 : MonoBehaviour
     [SerializeField] private TMP_InputField answerInputField;
     public void CaseWin()
     {
-        if (answerInputField.text != null && (answerInputField.text == "2 30")) 
+        if (answerInputField.text != null && (answerInputField.text == "2, 30")) 
         {
             gameObject.GetComponent<WinCase>().WinCasePlayerPrefs();
         }
