aab9b8ffdf0777901fd2623121a9af2f0c3d088d
Фёдор <fedpr.pavlov.02@mail.ru>
2023-05-01 17:50:21 +0700

    Fixes

--- Assets/Scripts/CaseScripts/Cases/e1m4/WinCaseE1M4.cs
+++ Assets/Scripts/CaseScripts/Cases/e1m4/WinCaseE1M4.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class WinCaseE1M4 : MonoBehaviour
 {   
     [SerializeField] private GameObject grid;
-    [SerializeField] private List<GameObject> elementsPosition;
+    [SerializeField] private List<GameObject> elementsPosition1;
+    [SerializeField] private List<GameObject> elementsPosition2;
     public void CheckParts()
     {
         var gridTransform = grid.GetComponent<Transform>();
         bool checkCondition = false;
         for (int i = 0; i < gridTransform.childCount; i++) 
         {
-            if (gridTransform.GetChild(i).GetChild(0) != elementsPosition[i].transform)
+            if (gridTransform.GetChild(i).GetChild(0) != elementsPosition1[i].transform)
             {
                 checkCondition = false;
                 print(checkCondition);
@@ -22,6 +23,23 @@ public class WinCaseE1M4 : MonoBehaviour
             {
                 checkCondition = true;
                 print(checkCondition);
+            }
+        }
+        if (checkCondition == false)
+        {
+            for (int i = 0; i < gridTransform.childCount; i++)//от 7 т.к. первый ребенок - это задний фон
+            {
+                if (gridTransform.GetChild(i).GetChild(0) != elementsPosition2[i].transform)
+                {
+                    checkCondition = false;
+                    print(checkCondition);
+                    break;
+                }
+                else
+                {
+                    checkCondition = true;
+                    print(checkCondition);
+                }
             }
         }
         if (checkCondition == true)
