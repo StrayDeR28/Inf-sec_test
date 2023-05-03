6315a3f4c259b1ea2e706d2d0ececa0cf8c15c84
Фёдор <fedpr.pavlov.02@mail.ru>
2023-05-03 19:45:12 +0700

    Cool e6m4 fixes

--- Assets/Scripts/CaseScripts/Cases/e6m4/WinCaseE6M4.cs
+++ Assets/Scripts/CaseScripts/Cases/e6m4/WinCaseE6M4.cs
@@ -6,6 +6,7 @@ public class WinCaseE6M4 : MonoBehaviour
 {
     [SerializeField] private GameObject dishes;
     [SerializeField] private List<GameObject> dishesPosition;
+    [SerializeField] private List<GameObject> dishesPosition2;
     public void CheckDishesPosition()
     {
         var dishesTransform = dishes.GetComponent<Transform>();
@@ -22,6 +23,23 @@ public class WinCaseE6M4 : MonoBehaviour
             {
                 checkCondition = true;
                 print(checkCondition);
+            }
+        }
+        if (checkCondition == false)
+        {
+            for (int i = 0; i < dishesTransform.childCount; i++)
+            {
+                if (dishesTransform.GetChild(i).GetChild(0) != dishesPosition2[i].transform)
+                {
+                    checkCondition = false;
+                    print(checkCondition);
+                    break;
+                }
+                else
+                {
+                    checkCondition = true;
+                    print(checkCondition);
+                }
             }
         }
         if (checkCondition == true)
