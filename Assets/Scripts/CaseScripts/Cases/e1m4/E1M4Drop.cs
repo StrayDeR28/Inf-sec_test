573060cac71dbdb24d3300c31701493daf9e6dab
Фёдор <fedpr.pavlov.02@mail.ru>
2023-05-03 20:30:56 +0700

    Fixed e1m4 crush

--- Assets/Scripts/CaseScripts/Cases/e1m4/E1M4Drop.cs
+++ Assets/Scripts/CaseScripts/Cases/e1m4/E1M4Drop.cs
@@ -11,13 +11,16 @@ public class E1M4Drop : MonoBehaviour, IDropHandler
     {
         if (transform.childCount > 0)
         {
-            var oldElementTransform = transform.GetChild(0);//смена строк
-            oldElementTransform.SetParent(eventData.pointerDrag.GetComponent<E1M4Drag>().GetTransform());
-            oldElementTransform.localPosition = Vector3.zero;
+            if (eventData.pointerDrag.GetComponent<E1M4Drag>() == true)
+            {
+                var oldElementTransform = transform.GetChild(0);//смена строк
+                oldElementTransform.SetParent(eventData.pointerDrag.GetComponent<E1M4Drag>().GetTransform());
+                oldElementTransform.localPosition = Vector3.zero;
 
-            var newElementTransform = eventData.pointerDrag.transform;
-            newElementTransform.SetParent(transform);
-            newElementTransform.localPosition = Vector3.zero;
+                var newElementTransform = eventData.pointerDrag.transform;
+                newElementTransform.SetParent(transform);
+                newElementTransform.localPosition = Vector3.zero;
+            }
         }
     }
 }
